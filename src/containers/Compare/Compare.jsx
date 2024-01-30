91c214a64898aa6e67680421ee9c18cd980e6cc1
Sachin <pathaksachin876@gmail.com>
2024-01-30 10:24:12 +0530

    ...

--- src/containers/Compare/Compare.jsx
+++ src/containers/Compare/Compare.jsx
@@ -3,6 +3,14 @@ import './Compare.css'
 function Compare() {
   return (
     <>
+    <div className="cli-header" id="cli-mainHeader">
+   
+   <p className="cli-head1">We help people</p>
+ 
+   <div className="cli-para">
+     <p className="cli-para1">Connect With Us The Industries Specialists And Get The Most Optimised Solution</p>
+   </div>
+ </div>
     <div class='compare-box'>
       <div class='container-card'>
           <div class='sect-first'>
@@ -13,6 +21,18 @@ function Compare() {
             <p class='heading-description'>Individual Practitioner</p>
             <p class='heading-description'>Clinic/Hospitals</p>
             <p class='heading-description'>Lab/DME</p>
+            <p class='heading-description'>Medical Billing</p>
+            </div>
+          </div>
+      </div>
+      <div className='container-card'>
+      <div class='sect-first'>
+            <div class='heading-first'>
+             RPO
+            </div>
+            <div class='heading-align'>
+            <p class='heading-description'>IT/ITes</p>
+            <p class='heading-description'>Customer Support</p>
             <p class='heading-description'>Medical Billing Companies</p>
             </div>
           </div>
@@ -20,54 +40,36 @@ function Compare() {
       <div className='container-card'>
       <div class='sect-first'>
             <div class='heading-first'>
-             US RCM
+             DIGITAL SOLUTIONS
             </div>
             <div class='heading-align'>
-            <p class='heading-description'>Individual Practitioner</p>
-            <p class='heading-description'>Clinic/Hospitals</p>
-            <p class='heading-description'>Lab/DME</p>
-            <p class='heading-description'>Medical Billing Companies</p>
+            <p class='heading-description'>Healthcare </p>
+            <p class='heading-description'>Constructions</p>
+            <p class='heading-description'>Fitness</p>
+            <p class='heading-description'>All Industries</p>
             </div>
           </div>
       </div>
-      <div className='container-card'>
-      <div class='sect-first'>
-            <div class='heading-first'>
-             US RCM
-            </div>
-            <div class='heading-align'>
-            <p class='heading-description'>Individual Practitioner</p>
-            <p class='heading-description'>Clinic/Hospitals</p>
-            <p class='heading-description'>Lab/DME</p>
-            <p class='heading-description'>Medical Billing Companies</p>
-            </div>
-          </div>
-      </div>
-      </div>
-      <div class='compareBox'>
       <div class='container-card'>
       <div class='sect-first'>
             <div class='heading-first'>
-            US RCM
+            Business Consulting
             </div>
             <div class='heading-align'>
-            <p class='heading-description'>Individual Practitioner</p>
-            <p class='heading-description'>Clinic/Hospitals</p>
-            <p class='heading-description'>Lab/DME</p>
-            <p class='heading-description'>Medical Billing Companies</p>
+            <p class='heading-description'>Product Based Companies</p>
+            <p class='heading-description'>Service Industries</p>
             </div>
           </div>
       </div>
       <div className='container-card'>
       <div class='sect-first'>
             <div class='heading-first'>
-             US RCM
+             Training Solutions
             </div>
             <div class='heading-align'>
-            <p class='heading-description'>Individual Practitioner</p>
-            <p class='heading-description'>Clinic/Hospitals</p>
-            <p class='heading-description'>Lab/DME</p>
-            <p class='heading-description'>Medical Billing Companies</p>
+            <p class='heading-description'>Corporate Training</p>
+            <p class='heading-description'>College Training</p>
+            <p class='heading-description'>Fitness Training</p>
             </div>
           </div>
       </div>
